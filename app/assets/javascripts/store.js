895fee4a6d2d947f41817c6e07901ba02fbd3f96
malchak <michaelmalchak@gmail.com>
2013-10-16 11:56:28 -0700

    Setup Ember-Data RESTAdapter

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,6 +1,5 @@
 // http://emberjs.com/guides/models/defining-a-store/
 
-App.Store = DS.Store.extend({
-  revision: 13,
-  adapter: DS.RESTAdapter.create()
+DS.RESTAdapter.reopen({
+	namespace: 'api/v1'
 });
